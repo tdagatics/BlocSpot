d38f142d5d2e29bbe7d4889d63235aab528919cc
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-30 10:47:08 -0700

    Initial Commit

---
+++ BlocSpot_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocSpot_2
+//
+//  Created by Anthony Dagati on 10/30/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocSpot_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocSpot_2
+//
+//  Created by Anthony Dagati on 10/30/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
